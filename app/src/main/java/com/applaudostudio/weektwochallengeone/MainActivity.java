f9c8161a00235ca0089663e6c294d7f29ec7b904
Jose <jose01.arteaga@gmail.com>
2018-09-05 12:07:42 -0600

    Fix
    Delete warnings

--- app/src/main/java/com/applaudostudio/weektwochallengeone/MainActivity.java
+++ app/src/main/java/com/applaudostudio/weektwochallengeone/MainActivity.java
@@ -1,23 +1,20 @@
 package com.applaudostudio.weektwochallengeone;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageView callButton;
-    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView callButton;
+
         callButton = findViewById(R.id.imageViewCall);
 
         callButton.setOnClickListener(this);
@@ -27,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+         Intent intent;
+
         switch (v.getId()) {
             case R.id.imageViewCall:
                 intent = new Intent(Intent.ACTION_DIAL);
