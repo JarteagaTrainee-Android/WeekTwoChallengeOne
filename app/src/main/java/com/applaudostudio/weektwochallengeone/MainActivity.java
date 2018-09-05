4cd7ea055e3fa1c22cc7f0e4f593277dfa62a7b2
Jose <jose01.arteaga@gmail.com>
2018-09-05 11:47:28 -0600

    Final
    All functions

--- app/src/main/java/com/applaudostudio/weektwochallengeone/MainActivity.java
+++ app/src/main/java/com/applaudostudio/weektwochallengeone/MainActivity.java
@@ -1,13 +1,40 @@
 package com.applaudostudio.weektwochallengeone;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView callButton;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callButton = findViewById(R.id.imageViewCall);
+
+        callButton.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageViewCall:
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+"+503 79866986"));
+                startActivity(intent);
+        }
+    }
+
+
+
 }
